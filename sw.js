3c48a8e85ff6061c03ed49965295172ab4897840
spainclaro-debug <spainclaro@gmail.com>
2026-04-28 03:22:52 +0800

    Update files before rebase

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker: Installed');
-});
-
-self.addEventListener('fetch', (e) => {
-  // Simple pass-through for local Termux files
-  e.respondWith(fetch(e.request));
-});
